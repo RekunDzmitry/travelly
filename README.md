7a7b059666cf1a7d490127ddc7733259d248b505
RekunDzmitry <rekundzmitry@gmail.com>
2023-03-12 10:52:45 +0100

    delete website

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # travelly
 Repository for travel website
 
-
 # usage
 run main.py
+
+
+# plan
+
+1. Develop home page
+2. Develop a city page with a map and description
+3. Add routes with lines on the map
+4. Add integration with chatGPT
--- website/static/index.js
+++
@@ -1,9 +0,0 @@
-function deleteTravel(id) {
-    // send delete request to server
-    fetch("/delete-travel", {
-        method: 'POST',
-        body: JSON.stringify({ travelId: id })
-    }).then((_res) => {
-        window.location.href = '/';
-    })
-}
